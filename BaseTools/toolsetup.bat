323cb9bf381b1d9d3abf8e59e4e0436ef7a09299
Yunhua Feng <yunhuax.feng@intel.com>
2018-09-07 10:51:37 +0800

    BaseTools: Update toolsetup.bat for python3 version check
    Update toolsetup.bat for python3 version check
    
    Cc: Liming Gao <liming.gao@intel.com>
    Cc: Yonghong Zhu <yonghong.zhu@intel.com>
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Yunhua Feng <yunhuax.feng@intel.com>

--- BaseTools/toolsetup.bat
+++ BaseTools/toolsetup.bat
@@ -303,36 +303,25 @@ goto end
     if EXIST "%PYTHON_HOME%" (
       set PYTHON3=%PYTHON_HOME%\python.exe
     )
-    goto check_python_version
   ) else (
-    echo.
-    goto check_py_install
+    set PYTHON3=py -3
   )
 :check_python_version
-  FOR /F "TOKENS=1,2" %%i IN ('%PYTHON3% --version') DO set VERSION=%%j
-  if %VERSION% LSS 3.6 (
-     echo.
-     echo !!! ERROR !!!  python version need larger than or equal 3.6.
-     echo.
-     goto end
-  )
-  goto check_freezer_path
-:check_py_install
-   py -3 --version >NUL 2>&1
+    %PYTHON3% --version >NUL 2>&1
    if %ERRORLEVEL% NEQ 0 (
      echo.
-     echo !!! ERROR !!!  Python3 not install.
+     echo !!! ERROR !!!  %PYTHON3% not install.
      echo.
      goto end
    )
-   FOR /F "TOKENS=1,2" %%i IN ('py -3 --version') DO set VERSION=%%j
-   if %VERSION% LSS 3.6 (
+  FOR /F "TOKENS=1,2" %%i IN ('%PYTHON3% --version') DO set VERSION=%%j
+  if /I "%VERSION%" LSS "3.6" (
      echo.
-     echo !!! ERROR !!!  python version need larger than or equal 3.6.
+     echo !!! ERROR !!!  python version should greater than or equal to version 3.6.
      echo.
      goto end
   )
-  set PYTHON3=py -3
+
 :check_freezer_path
   @REM We have Python, now test for FreezePython application
   if not defined PYTHON_FREEZER_PATH (
